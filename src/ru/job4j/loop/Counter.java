2704fcc7fc27cb3634b71fda8a139ea8cc79d57a
Tatyana <anatoliy.gromov.service@gmail.com>
2021-03-26 21:11:32 +0300

    Исправил сравнение в цикле for метода sum.

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -4,7 +4,7 @@ public class Counter {
 
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = start; i < finish; i++) {
+        for (int i = start; i <= finish; i++) {
             sum = sum + i;
         }
         return sum;
@@ -21,7 +21,7 @@ public class Counter {
     }
 
     public static void main(String[] args) {
-        System.out.println(sum(0, 10));
+        System.out.println(sum(0, 5));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
         System.out.println(sum(4, 14));
