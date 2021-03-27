e59ba69925ef3d2b4c3567d66f114ac59b149f00
Tatyana <anatoliy.gromov.service@gmail.com>
2021-03-27 22:02:22 +0300

    Добавил метод monoVertical и тесты для него.

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,12 +4,24 @@ public class MatrixCheck {
 
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-            for (int j = 0; j < board[row].length; j++) {
-                if (board[row][j] != 'X') {
+            for (int i = 0; i < board[row].length; i++) {
+                if (board[row][i] != 'X') {
                     result = false;
                     break;
                 }
             }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -17,4 +17,16 @@ public class MatrixCheckTest {
         Assert.assertTrue(result);
     }
 
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
+        Assert.assertTrue(result);
+    }
+
 }
